7afa335a31d066a29f7b5e187c88bdc705f00667
mrparalon <mr.paralon@gmail.com>
2019-06-02 23:18:37 +0300

    work on task 3 homework 1

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response, redirect
 from django.urls import reverse
 from django.conf import settings
+from urllib.parse import urlencode
 from csv import DictReader
 from itertools import islice
 import os
@@ -9,12 +10,11 @@ import os
 def index(request):
     return redirect(reverse(bus_stations))
 
-def get_data_from_csv(data_file, page):
+def get_data_from_csv(data_file, page, list_size=10):
     with open(data_file, encoding='cp1251') as csv_file:
         csv_reader = DictReader(csv_file)
-        # data = csv_reader[(page - 1 ) * 10, (page - 1) * 10 - 10]
-        start = (page - 1 ) * 10
-        stop = (page - 1) * 10 + 10
+        start = (page - 1 ) * list_size
+        stop = (page - 1) * list_size + list_size
         data = []
         for row in islice(csv_reader, start, stop):
             data.append(row)
@@ -24,17 +24,24 @@ def get_data_from_csv(data_file, page):
 
 def bus_stations(request):
     file_path = os.path.join(settings.BUS_STATION_CSV)
-    data = get_data_from_csv(file_path, 2)
-    bus_stations = []
+    current_page = request.GET.get('page')
+    if not current_page:
+        current_page = 1
+    current_page = int(current_page)
+    data = get_data_from_csv(file_path, current_page)
+    bus_stations_list = []
     for station in data:
         station_info_for_site = {'Name': station['Name'],
                                  'Street': station['Street'],
                                  'District': station['District']}
-        bus_stations.append(station_info_for_site)
+        bus_stations_list.append(station_info_for_site)
+    url_begin = reverse(bus_stations)
+    next_page = current_page + 1
+    print(url_begin)
     return render_to_response('index.html', context={
-        'bus_stations': bus_stations,
-        'current_page': 1,
+        'bus_stations': bus_stations_list,
+        'current_page': current_page,
         'prev_page_url': None,
-        'next_page_url': 'bus_stations/?page=2',
+        'next_page_url': f'{url_begin}?{urlencode({"page": next_page})}'
     })
 
