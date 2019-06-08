8dbdc1ea628a2120e140a53d469ab5510aca348f
mrparalon <mrparalon@mr.paralon-Kubuntu>
2019-06-08 23:35:58 +0300

    Homework 2 task 3 started, format_date ready

--- dynamic-templates/task3/app/templatetags/news_filters.py
+++ dynamic-templates/task3/app/templatetags/news_filters.py
@@ -1,4 +1,5 @@
 from django import template
+import datetime
 
 
 register = template.Library()
@@ -6,7 +7,15 @@ register = template.Library()
 
 @register.filter
 def format_date(value):
-    # Ваш код
+    now = datetime.datetime.utcnow()
+    date = datetime.datetime.utcfromtimestamp(value)
+    time_dif = now - date
+    if time_dif < datetime.timedelta(seconds=600):
+        value = 'только что'
+    elif datetime.timedelta(hours=1) <= time_dif <= datetime.timedelta(hours=24):
+        value = f"{str(int(time_dif.seconds/3600))} часов назад"
+    else:
+        value = date.strftime('%Y-%m-%d')
     return value
 
 
