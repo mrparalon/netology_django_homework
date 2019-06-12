781bb204a585f6c0b06400bf9a4c3b34102c4a13
mrparalon <mrparalon@mr.paralon-Kubuntu>
2019-06-12 20:08:32 +0300

    Homework 2 fixed

--- dynamic-templates/task1/app/templatetags/color_filters.py
+++ dynamic-templates/task1/app/templatetags/color_filters.py
@@ -5,18 +5,20 @@ register = template.Library()
 
 @register.filter
 def inflation_color(inflation, column, name='inflation_color'):
-    # Мне кажется, if-ов многовато, но лучше я не придумал
     color = None
     if inflation:
-        inflation = float(inflation)
-        if inflation < 0:
-            color = '#00e86d'
-        elif 1 < inflation <= 2:
-            color = '#ffa0a0'
-        elif 2 < inflation <= 5:
-            color = '#ff3d3d'
-        elif inflation > 5:
-            color = '#9f0000'
+        if column == 'Год':
+            inflation = int(inflation)
+        else:
+            inflation = float(inflation)
+            if inflation < 0:
+                color = '#00e86d'
+            elif 1 < inflation <= 2:
+                color = '#ffa0a0'
+            elif 2 < inflation <= 5:
+                color = '#ff3d3d'
+            elif inflation > 5:
+                color = '#9f0000'
     else:
         inflation = '-'
     if column == 'Суммарная':
--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -9,6 +9,7 @@ def inflation_view(request):
     file_path = os.path.join(os.path.dirname(__file__), '..', 'inflation_russia.csv')
     with open(file_path) as csv_file:
         csv_data = list(DictReader(csv_file, delimiter=';'))
-    context = {'table': csv_data}
+    headers = csv_data[0].keys()
+    context = {'table': csv_data, 'csv_headers': headers}
     return render(request, template_name,
                   context)
