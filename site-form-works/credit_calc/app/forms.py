bf24793ecfc21b60938f45f8695b0cb59f1480a8
mrparalon <mrparalon@mr.paralon-Kubuntu>
2019-06-24 21:18:31 +0300

    Homework 5 task 1 finished

--- site-form-works/credit_calc/app/forms.py
+++ site-form-works/credit_calc/app/forms.py
@@ -2,17 +2,32 @@ from django import forms
 
 
 class CalcForm(forms.Form):
-    initial_fee = forms.IntegerField(label="Стоимость товара")
-    rate = forms.CharField(label="Процентная ставка")
-    months_count = forms.IntegerField(label="Срок кредита в месяцах")
+    initial_fee = forms.IntegerField(label="Стоимость товара",
+                                     min_value=1)
+    rate = forms.IntegerField(label="Процентная ставка",
+                              min_value=1)
+    months_count = forms.IntegerField(label="Срок кредита в месяцах",
+                                      min_value=1)
 
     def clean_initial_fee(self):
         # валидация одного поля, функция начинающаяся на `clean_` + имя поля
         initial_fee = self.cleaned_data.get('initial_fee')
         if not initial_fee or initial_fee < 0:
-            raise forms.ValidationError("Стоимость товара не может быть отрицательной")
+            raise forms.ValidationError("Стоимость товара не может быть\
+                                         отрицательной")
         return initial_fee
 
-    def clean(self):
-        # общая функция валидации
-        return self.cleaned_data
+    def clean_rate(self):
+        rate = self.cleaned_data.get('rate')
+        if not rate or rate < 8:
+            raise forms.ValidationError('Не обманывайте, не бывает таких низких\
+                                         процентов. Введите реальные проценты.')
+        return rate
+
+    def clean_months_count(self):
+        months_count = self.cleaned_data['months_count']
+        if not months_count or months_count <= 2:
+            raise forms.ValidationError('Зачем вам кредит на такой короткий\
+                                         срок? Лучше подкопите.')
+        return months_count
+
--- site-form-works/credit_calc/app/views.py
+++ site-form-works/credit_calc/app/views.py
@@ -5,10 +5,18 @@ from .forms import CalcForm
 
 def calc_view(request):
     template = "app/calc.html"
-
-    form = CalcForm
+    form = CalcForm(request.GET)
+    common_result, result = None, None
+    if form.is_valid():
+        initial_fee, rate, months_count = (form.cleaned_data['initial_fee'], 
+                                           form.cleaned_data['rate'],
+                                           form.cleaned_data['months_count'])
+        common_result = initial_fee + initial_fee * rate / 100
+        result = common_result / months_count
     context = {
-        'form': form
+        'form': form,
+        'common_result': common_result,
+        'result': result
     }
 
     return render(request, template, context)
